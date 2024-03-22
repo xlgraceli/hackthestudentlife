feef18c525b9db673ac535aad39cf20e7433afa4
xlgraceli <xlgraceli@gmail.com>
2024-03-22 11:19:08 -0400

    something

---
+++ server/index.js
@@ -0,0 +1,32 @@
+// server.js
+const express = require('express')
+const { CohereClient } = require('cohere-ai')
+
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const uri =
+  'mongodb+srv://xlgraceli:<password>@questionbank.peaeot6.mongodb.net/'
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+
+async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect()
+      // Send a ping to confirm a successful connection
+      await client.db('admin').command({ ping: 1 })
+      console.log(
+        'Pinged your deployment. You successfully connected to MongoDB!'
+      )
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close()
+    }
+  }
+  run().catch(console.dir)
